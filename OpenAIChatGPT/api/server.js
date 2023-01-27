2a3e4a33b19a3da3fe619ad8ee35c095718e939b
Aman <paman7681@gmail.com>
2023-01-27 20:27:05 +0530

    commit

--- OpenAIChatGPT/api/server.js
+++ OpenAIChatGPT/api/server.js
@@ -25,7 +25,7 @@ app.post("/", async (req,res) => {
         const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: req.body.input,
-            temperature: 0,
+            temperature: 0.4,
             max_tokens: 4000,
             top_p: 1,
             frequency_penalty: 0.4,
